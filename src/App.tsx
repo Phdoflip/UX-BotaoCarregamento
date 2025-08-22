efe67bb12742f2b735c29a9050dfd506014affa6
Phdoflip <antrodossantos@gmail.com>
2025-08-22 10:36:15 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  function handleClick() {
+    setLoading(true);
+    setMessage("");
+
+    // Simula uma requisição (ex: login, salvar dados)
+    setTimeout(() => {
+      setLoading(false);
+      setMessage("Ação concluída com sucesso!");
+    }, 2000);
+  }
+
+  return (
+    <div>
+      <h1>Exercício: Botão de Carregamento</h1>
+      <br />
+      <p>Esse código simula uma operação de envio de dados que demora 2 segundos. </p>
+      <button
+        className="btn"
+        onClick={handleClick}
+      >
+        {loading ? "Enviar" : "Enviar"}
+      </button>
+      <br />
+      {message && <p className="success">{message}</p>}
+    </div>
+  );
+}
+
+export default App;
